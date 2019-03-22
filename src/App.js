40b08115c34232e14c3a3e5ca9d6b5c6f90bd170
Chris Wright <cmwright33@gmail.com>
2019-03-22 11:03:42 -0400

    test search query

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import BookShelf from './BookShelf.js'
+import BookItem from './BookItem.js'
 import * as BooksAPI from './BooksAPI'
 import './App.css'
 
@@ -14,62 +15,7 @@ class BooksApp extends React.Component {
     query: '',
     showSearchPage: false,
     books: [],
-
-
-
-    // {
-
-    //   url : "http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api",
-    //   author : "Harper Lee",
-    //   title : "To Kill a Mockingbird",
-    //   shelf : "currentlyReading"
-    // },
-    // {
-
-    //   url : "http://books.google.com/books/content?id=yDtCuFHXbAYC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72RRiTR6U5OUg3IY_LpHTL2NztVWAuZYNFE8dUuC0VlYabeyegLzpAnDPeWxE6RHi0C2ehrR9Gv20LH2dtjpbcUcs8YnH5VCCAH0Y2ICaKOTvrZTCObQbsfp4UbDqQyGISCZfGN&source=gbs_api",
-    //   author : "Orson Scott Card",
-    //   title : "Ender's Game",
-    //   shelf : "currentlyReading"
-    // },
-    //   {
-
-    //   url : "http://books.google.com/books/content?id=uu1mC6zWNTwC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73pGHfBNSsJG9Y8kRBpmLUft9O4BfItHioHolWNKOdLavw-SLcXADy3CPAfJ0_qMb18RmCa7Ds1cTdpM3dxAGJs8zfCfm8c6ggBIjzKT7XR5FIB53HHOhnsT7a0Cc-PpneWq9zX&source=gbs_api",
-    //   author : "David McCullough",
-    //   title : "1776",
-    //   shelf : "wantToRead"
-    // },
-
-    // {
-
-    //   url :   "http://books.google.com/books/content?id=wrOQLV6xB-wC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72G3gA5A-Ka8XjOZGDFLAoUeMQBqZ9y-LCspZ2dzJTugcOcJ4C7FP0tDA8s1h9f480ISXuvYhA_ZpdvRArUL-mZyD4WW7CHyEqHYq9D3kGnrZCNiqxSRhry8TiFDCMWP61ujflB&source=gbs_api",
-    //   author : "J.K. Rowling",
-    //   title : "Harry Potter and the Sorcerer's Stone",
-    //   shelf : "wantToRead"
-    // },
-
-    //   {
-
-    //   url :   "http://books.google.com/books/content?id=pD6arNyKyi8C&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE70Rw0CCwNZh0SsYpQTkMbvz23npqWeUoJvVbi_gXla2m2ie_ReMWPl0xoU8Quy9fk0Zhb3szmwe8cTe4k7DAbfQ45FEzr9T7Lk0XhVpEPBvwUAztOBJ6Y0QPZylo4VbB7K5iRSk&source=gbs_api",
-    //   author : "J.R.R. Tolkien",
-    //   title : "The Hobbit",
-    //   shelf : "read"
-    // },
-    //   {
-
-    //   url :   "http://books.google.com/books/content?id=1q_xAwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE712CA0cBYP8VKbEcIVEuFJRdX1k30rjLM29Y-dw_qU1urEZ2cQ42La3Jkw6KmzMmXIoLTr50SWTpw6VOGq1leINsnTdLc_S5a5sn9Hao2t5YT7Ax1RqtQDiPNHIyXP46Rrw3aL8&source=gbs_api",
-    //   author : "Dr. Seuss",
-    //   title : "Oh, the Places You'll Go!",
-    //   shelf : "read"
-    // },
-    // {
-
-    //   url :   "http://books.google.com/books/content?id=32haAAAAMAAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72yckZ5f5bDFVIf7BGPbjA0KYYtlQ__nWB-hI_YZmZ-fScYwFy4O_fWOcPwf-pgv3pPQNJP_sT5J_xOUciD8WaKmevh1rUR-1jk7g1aCD_KeJaOpjVu0cm_11BBIUXdxbFkVMdi&source=gbs_api",
-    //   author : "Mark Twain",
-    //   title : "The Adventures of Tom Sawyer",
-    //   shelf : "read"
-    // },
-
-    
+    bookSearch: [], 
 
   }
 
@@ -84,6 +30,20 @@ class BooksApp extends React.Component {
     }
 
 
+  setQuery = (event) => {
+      this.setState( { query: event.target.value })
+  }
+
+  handleInput = (event) => {
+
+    if (event.key === "Enter") {
+      BooksAPI.search(this.state.query)
+       .then( (books) => {
+          console.log(books)
+          // this.setState( { bookSearch: books})
+       })
+    }
+  }
 
 
 
@@ -117,7 +77,8 @@ class BooksApp extends React.Component {
                   However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
                   you don't find a specific author or title. Every search is limited by search terms.
                 */}
-                <input type="text" placeholder="Search by title or author"/>
+
+                <input type="text" onChange={ (e) => {this.setQuery(e) }} onKeyPress = { (e) => {this.handleInput(e)} } value={this.state.query} placeholder="Search by title or author"/>
 
               </div>
             </div>
